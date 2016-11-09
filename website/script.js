ca9ba895100190e3cbeb5944dead44760238f74c
kelvin xie <kelvin xie>
2016-11-09 16:59:26 -0500

    Adding answer checking functions, not tested

--- website/script.js
+++ website/script.js
@@ -27,19 +27,82 @@ function createMultiplicationQuestion($container, number) {
 		
 		var int1 = randInt(1, 99);
 		var int2 = randInt(1, 99);
+		var int3 = 0; //Stores value being added to radiobutton and span
+		
 		
 		var $question = $("<p>").html("Calculate: " + int1 + " * " + int2);
 		var $opt1 = $('<input type="radio" name=' + qid + ' />').prop( "checked", true);
-		var $val1 = $("<span>").html(int1 * int2);
+		int3 = int1 * int2;
+		$opt1.value = int3;
+		var $val1 = $("<span>").html(int3);
+		
 		var $opt2 = $('<input type="radio" name=' + qid + ' />');
-		var $val2 = $("<span>").html(int1 * int2 + randInt(1, 99));
+		int3 = int1 * int2 + randInt(1, 99);
+		$opt2.value = int3;
+		var $val2 = $("<span>").html(int3);
+		
 		var $opt3 = $('<input type="radio" name=' + qid + ' />');
-		var $val3 = $("<span>").html(int1 * int2 + randInt(1, 99));
+		int3 = int1 * int2 + randInt(1, 99);
+		$opt3.value = int3;
+		var $val3 = $("<span>").html(int3);
+		
 		var $opt4 = $('<input type="radio" name=' + qid + ' />');
-		var $val4 = $("<span>").html(int1 * int2 - randInt(1, 99));
+		int3 = int1 * int2 - randInt(1, 99);
+		$opt4.value = int3;
+		var $val4 = $("<span>").html(int3);
 		
 		$section.append($title, $question, $opt1, $val1, $opt2, $val2, $opt3, $val3, $opt4, $val4);
 		$container.append($section); //Name of container
+	}
+}
+
+function checkAnswers($container){
+	//jquery of sections
+	var $sections = $container.find("section");
+	var $sect;
+	var $radioButtons;
+	var answer;
+	var question;
+	var $p;
+	
+	var score = 0;
+	
+	//Go through every section in the container
+	for(var x = 0; x < $sections.length; x++){
+		$sect = $sections[x];
+		//jquery of radiobuttons for a specific section
+		$radioButtons = $sect.find("radio");
+		//Find the checked radiobutton
+		for(var y = 0; y < $radioButtons.length; y++){
+			if($radioButtons[y].prop("checked")){
+				answer = $radioButtons[y].value;
+			}
+		}
+		//Find the question
+		$p = $sect.find("p");
+		question = $p[0].text();
+		
+		score = score + checkAnswer(question, answer);
+	}
+	return score;
+}
+
+function checkAnswer(question, answer){
+	var x1;
+	var x2;
+	
+	//Format of question obtained from p is:
+	//Calculate: x1 * x2
+	var res = question.split(" ");
+	
+	x1 = parseInt(res[1]);
+	x2 = parseInt(res[3]);
+	
+	if(answer == x1 * x2){
+		return 1;
+	}
+	else{
+		return 0;
 	}
 }
 
